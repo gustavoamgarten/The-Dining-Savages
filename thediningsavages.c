764acf413d80c21bc7d8585c68d533d834d196a9
Gustavo Amgarten <guamgarten@gmail.com>
2014-04-20 21:04:34 -0300

    Adicionado suporte a Curses e início de animação.

--- thediningsavages.c
+++ thediningsavages.c
@@ -5,28 +5,44 @@
 #include <semaphore.h>
 #include <curses.h>
  
-#define MAX_SERVINGS 30
-#define SAVAGES_NUMBER 30
+#define MAX_SERVINGS 5
+#define SAVAGES_NUMBER 3
  
 volatile int servings = 0;
+volatile int savagesInit = 0;
  
 sem_t emptyPot;
 sem_t fullPot;
  
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void putServingsInPot(){
+	//clear();
+	printw("Servindo o pote.");
+	refresh();
+	sleep(1);
+}
+
+void getServingFromPot(int id){
+	//clear();
+	printw("Selvagem %d pegando comida do pote.", id);
+	refresh();
+	sleep(1);
+}
  
 void* cook(void *v) {
-        while (true) {
+        while (1) {
                 sem_wait(&emptyPot);
  
-                //putServingsInPot();        /*ANIMAÇÃO DE ENCHER O POTE AQUI (FAZER EM FUNÇÃO PARA FICAR MAIS ORGANIZADO O CÓDIGO)*/
+                putServingsInPot();        /*ANIMAÇÃO DE ENCHER O POTE AQUI (FAZER EM FUNÇÃO PARA FICAR MAIS ORGANIZADO O CÓDIGO)*/
  
                 sem_post(&fullPot);
         }
 }
  
 void* savage(void *v) {
-        while (true) {
+		int id = *(int *)v;
+        while (1) {
                 pthread_mutex_lock(&mutex);
                 if (servings == 0) {
                         sem_post(&emptyPot);
@@ -37,7 +53,7 @@ void* savage(void *v) {
  
                 servings--;
  
-                //getServingFromPot(); /*ANIMAÇÃO DE ESVAZIAR O POTE AQUI (FAZER EM FUNÇÃO PARA FICAR MAIS ORGANIZADO O CÓDIGO)*/
+                getServingFromPot(id); /*ANIMAÇÃO DE ESVAZIAR O POTE AQUI (FAZER EM FUNÇÃO PARA FICAR MAIS ORGANIZADO O CÓDIGO)*/
  
                 pthread_mutex_unlock(&mutex);
  
@@ -47,18 +63,29 @@ void* savage(void *v) {
 }
  
 int main() {
-		int i;
-		pthread_t thr_savages[SAVAGES_NUMBER];
-        pthread_t thr_cook;
- 
-        sem_init(&emptyPot, 0, 0);
-        sem_init(&fullPot, 0, 0);
- 
-        pthread_create(&thr_cook, NULL, cook, NULL);
- 
-        for (i = 0; i < SAVAGES_NUMBER; i++) {
-			pthread_create(&thr_savages[i], NULL, savage, NULL);
-		}
- 
-        return 0;
+	int i;
+	pthread_t thr_savages[SAVAGES_NUMBER];
+	pthread_t thr_cook;
+	
+	int savagesIds[SAVAGES_NUMBER];
+	for(i = 0; i < SAVAGES_NUMBER; i++){
+		savagesIds[i] = i;
+	}
+	
+	initscr(); /* Start curses mode  */
+
+	sem_init(&emptyPot, 0, 0);
+	sem_init(&fullPot, 0, 0);
+
+	pthread_create(&thr_cook, NULL, cook, NULL);
+
+	for (i = 0; i < SAVAGES_NUMBER; i++) {
+		pthread_create(&thr_savages[i], NULL, savage, (void *)&savagesIds[i]);
+	}
+	
+	getch(); // Se o usuário teclar qualquer tecla, o programa encerra (não precisa do join)
+
+	endwin(); /* End curses mode */
+
+	return 0;
 }
